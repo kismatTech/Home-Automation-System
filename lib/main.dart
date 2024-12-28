c04d13f47ea348b980de616b87e49115a0088b79
kismatTech <kismatduwadi123@gmai.com>
2024-12-28 21:09:15 +0545

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure proper initialization
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   runApp(const MyApp());
+  
 }
 
 class MyApp extends StatelessWidget {
